dc332efb3812dcc280b596732fea8514df7a469b
maryhosseiny <maryhosseini0@gmail.com>
2025-05-02 23:13:27 -0700

    changed button style to semi-transparent

--- bloom.py
+++ bloom.py
@@ -6,36 +6,52 @@ import sys
 pygame.init()
 
 screen_width = 800
-screen_height = 600
+screen_height = 533
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Grow with me")
+
+bg = pygame.image.load('img/back.png')
+background_img = pygame.transform.scale(bg, (screen_width, screen_height))
+
 
 white = (255, 255, 255)
 green = (100, 200, 100)
 gray = (170, 170, 170)
 
-font = pygame.font.SysFont('Arial', 36)
+font = pygame.font.SysFont('Bauhuas 93', 36)
 
-class Button:
-    def __init__(self, x, y, text):
+class GlassButton:
+    def __init__(self, x, y, width, height, text):
+        self.rect = pygame.Rect(x, y, width, height)
         self.text = text
-        self.image = font.render(text, True, white)
-        self.rect = self.image.get_rect(center = (x,y))
-        self.color = green
+        self.base_color = (255, 255, 255, 100) 
+        self.hover_color = (255, 255, 255, 140)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont('Arial', 30)
+        self.hovered = False
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect.inflate(20, 20), border_radius=12)
-        screen.blit(self.image, self.rect)
+    def draw(self, surface):
+        mouse_pos = pygame.mouse.get_pos()
+        self.hovered = self.rect.collidepoint(mouse_pos)
+
+        button_surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
+        color = self.hover_color if self.hovered else self.base_color
+        pygame.draw.rect(button_surface, color, button_surface.get_rect(), border_radius=15)
+        surface.blit(button_surface, self.rect.topleft)
+
+        text_surf = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        surface.blit(text_surf, text_rect)
 
     def is_clicked(self, pos):
         return self.rect.collidepoint(pos)
     
-start_button = Button(screen_width// 2, screen_height//2 - 60, "Start Growing")
-shelf_button = Button(screen_width// 2, screen_height//2 + 60, "Shelf")
+start_button = GlassButton(screen_width// 2-100, screen_height//2 -30,200, 60, "Start Growing")
+shelf_button = GlassButton(screen_width// 2 + 200, screen_height//2 + 170, 150, 50,"Shelf")
 
 running = True
 while running:
-    screen.fill(gray)
+    screen.blit(bg, (0,0))
 
     start_button.draw(screen)
     shelf_button.draw(screen)
